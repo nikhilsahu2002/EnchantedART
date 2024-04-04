0ff06b22c145d88e43b703c7025efad6a2f47039
NIkhil Sahu <117375980+nikhilsahu2002@users.noreply.github.com>
2024-04-04 23:26:21 +0530

    commit

--- src/views/view_product/index.jsx
+++ src/views/view_product/index.jsx
@@ -1,35 +1,35 @@
-import { ArrowLeftOutlined, LoadingOutlined } from '@ant-design/icons';
-import { ColorChooser, ImageLoader, MessageDisplay } from '@/components/common';
-import { ProductShowcaseGrid } from '@/components/product';
-import { RECOMMENDED_PRODUCTS, SHOP } from '@/constants/routes';
-import { displayMoney } from '@/helpers/utils';
+import { ArrowLeftOutlined, LoadingOutlined } from "@ant-design/icons";
+import { ColorChooser, ImageLoader, MessageDisplay } from "@/components/common";
+import { ProductShowcaseGrid } from "@/components/product";
+import { RECOMMENDED_PRODUCTS, SHOP } from "@/constants/routes";
+import { displayMoney } from "@/helpers/utils";
 import {
   useBasket,
   useDocumentTitle,
   useProduct,
   useRecommendedProducts,
-  useScrollTop
-} from '@/hooks';
-import React, { useEffect, useRef, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import Select from 'react-select';
+  useScrollTop,
+} from "@/hooks";
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Select from "react-select";
 
 const ViewProduct = () => {
   const { id } = useParams();
   const { product, isLoading, error } = useProduct(id);
   const { addToBasket, isItemOnBasket } = useBasket(id);
   useScrollTop();
-  useDocumentTitle(`View ${product?.name || 'Item'}`);
+  useDocumentTitle(`View ${product?.name || "Item"}`);
 
-  const [selectedImage, setSelectedImage] = useState(product?.image || '');
-  const [selectedSize, setSelectedSize] = useState('');
-  const [selectedColor, setSelectedColor] = useState('');
+  const [selectedImage, setSelectedImage] = useState(product?.image || "");
+  const [selectedSize, setSelectedSize] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
 
   const {
     recommendedProducts,
     fetchRecommendedProducts,
     isLoading: isLoadingFeatured,
-    error: errorFeatured
+    error: errorFeatured,
   } = useRecommendedProducts(6);
   const colorOverlay = useRef(null);
 
@@ -37,6 +37,7 @@ const ViewProduct = () => {
     setSelectedImage(product?.image);
   }, [product]);
 
+  console.log(selectedImage);
   const onSelectedSizeChange = (newValue) => {
     setSelectedSize(newValue.value);
   };
@@ -49,7 +50,11 @@ const ViewProduct = () => {
   };
 
   const handleAddToBasket = () => {
-    addToBasket({ ...product, selectedColor, selectedSize: selectedSize || product.sizes[0] });
+    addToBasket({
+      ...product,
+      selectedColor,
+      selectedSize: selectedSize || product.sizes[0],
+    });
   };
 
   return (
@@ -58,13 +63,11 @@ const ViewProduct = () => {
         <div className="loader">
           <h4>Loading Product...</h4>
           <br />
-          <LoadingOutlined style={{ fontSize: '3rem' }} />
+          <LoadingOutlined style={{ fontSize: "3rem" }} />
         </div>
       )}
-      {error && (
-        <MessageDisplay message={error} />
-      )}
-      {(product && !isLoading) && (
+      {error && <MessageDisplay message={error} />}
+      {product && !isLoading && (
         <div className="product-view">
           <Link to={SHOP}>
             <h3 className="button-link d-inline-flex">
@@ -80,8 +83,7 @@ const ViewProduct = () => {
                     className="product-modal-image-collection-wrapper"
                     key={image.id}
                     onClick={() => setSelectedImage(image.url)}
-                    role="presentation"
-                  >
+                    role="presentation">
                     <ImageLoader
                       className="product-modal-image-collection-img"
                       src={image.url}
@@ -91,7 +93,14 @@ const ViewProduct = () => {
               </div>
             )}
             <div className="product-modal-image-wrapper">
-              {selectedColor && <input type="color" disabled ref={colorOverlay} id="color-overlay" />}
+              {selectedColor && (
+                <input
+                  type="color"
+                  disabled
+                  ref={colorOverlay}
+                  id="color-overlay"
+                />
+              )}
               <ImageLoader
                 alt={product.name}
                 className="product-modal-image"
@@ -114,7 +123,9 @@ const ViewProduct = () => {
                 <Select
                   placeholder="--Select Size--"
                   onChange={onSelectedSizeChange}
-                  options={product.sizes.sort((a, b) => (a < b ? -1 : 1)).map((size) => ({ label: `${size} mm`, value: size }))}
+                  options={product.sizes
+                    .sort((a, b) => (a < b ? -1 : 1))
+                    .map((size) => ({ label: `${size} mm`, value: size }))}
                   styles={{ menu: (provided) => ({ ...provided, zIndex: 10 }) }}
                 />
               </div>
@@ -133,16 +144,21 @@ const ViewProduct = () => {
               <h1>{displayMoney(product.price)}</h1>
               <div className="product-modal-action">
                 <button
-                  className={`button button-small ${isItemOnBasket(product.id) ? 'button-border button-border-gray' : ''}`}
+                  className={`button button-small ${
+                    isItemOnBasket(product.id)
+                      ? "button-border button-border-gray"
+                      : ""
+                  }`}
                   onClick={handleAddToBasket}
-                  type="button"
-                >
-                  {isItemOnBasket(product.id) ? 'Remove From Basket' : 'Add To Basket'}
+                  type="button">
+                  {isItemOnBasket(product.id)
+                    ? "Remove From Basket"
+                    : "Add To Basket"}
                 </button>
               </div>
             </div>
           </div>
-          <div style={{ marginTop: '10rem' }}>
+          <div style={{ marginTop: "10rem" }}>
             <div className="display-header">
               <h1>Recommended</h1>
               <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
@@ -154,7 +170,10 @@ const ViewProduct = () => {
                 buttonLabel="Try Again"
               />
             ) : (
-              <ProductShowcaseGrid products={recommendedProducts} skeletonCount={3} />
+              <ProductShowcaseGrid
+                products={recommendedProducts}
+                skeletonCount={3}
+              />
             )}
           </div>
         </div>
